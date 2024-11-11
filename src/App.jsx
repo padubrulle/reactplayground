bce4066877dcf6e1dcddecf02359f49c914fcc33
padub <pierrealain.dubrulle@gmail.com>
2024-11-11 15:07:40 +0100

    data_flux tuto end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react"
+
+
 
 function App() {
-  return "Bonjour les gens"
+  // return "Bonjour les gens"
+
+  const [isTermAccepted, setChecked] = useState(true)
+    
+
+  return <form>
+    <CGUCheckbox checked={isTermAccepted} onCheck={setChecked}/>
+    <button disabled={!isTermAccepted}>Envoyer</button>
+  </form>
+}
+
+
+function CGUCheckbox ({onCheck, ...props}){
+  return <div>
+    <label>
+      <input 
+        type="checkbox" 
+        onChange={(e) => onCheck(e.target.checked)} 
+        {...props}/>
+      Accepter les CGU
+    </label>
+  </div>
 }
 
 export default App
