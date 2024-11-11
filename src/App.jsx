69f2480ad34bfa00c946680586b1b7b5f3135c08
padub <pierrealain.dubrulle@gmail.com>
2024-11-11 14:52:25 +0100

    fin tuto formulaires

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-
+import { useState } from "react"
 function App() {
-  return "Bonjour les gens"
+
+
+// const [firstname, setFirstname] = useState('John doe')
+// const handleChange = (e) => {
+//   setFirstname(e.target.value)
+// }
+// const reset = () => {
+//   setFirstname('')
+// }
+
+const [checked, setChecked] = useState(true)
+
+const handleCheckbox = () => {
+  setChecked(!checked)
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(new FormData(e.target))
+}
+  return <form onSubmit={handleSubmit}>
+    {/* <input type="text" name="firstname" value={firstname} onChange={handleChange}/> */}
+    {/* {firstname}
+    <button onClick={reset} type='button'>Reset</button> */}
+    <input type="text" name="firstname" defaultValue="Johnny cash"/>
+    <input type="checkbox" checked={checked} onChange={handleCheckbox}/>
+    <button disabled={!checked}>Envoyer</button>
+  </form>
 }
 
 export default App
