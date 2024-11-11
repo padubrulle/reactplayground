e0d6ac50b5d4679ef8a7ba34662ac3936aa31e28
padub <pierrealain.dubrulle@gmail.com>
2024-11-11 13:28:06 +0100

    End of syntaxe jsx tuto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
+import { Fragment } from "react"
+import Title from './Title.jsx'
+
+const title = "Bonjour les gens"
+const style = {
+  color: "red",
+  backgroundColor: 'blue'
+}
+let showTitle = true
+const todos = [
+  'Apprendre react',
+  'apprendre le JSX',
+  'Apprendre à créer des composants'
+]
+
+let hide = null
 
 function App() {
-  return "Bonjour les gens"
+
+  const handleClick = (e) => {
+    console.log(e)
+    alert('I clicked on title')
+  }
+
+  const displayTitle = () => {
+    showTitle = !showTitle;
+    showTitle ? hide = "hidden" : hide = null
+    console.log(hide)
+  }
+  return <Fragment>
+      {
+        showTitle ? 
+        <Title color='red' id="monid" class="MaClasscool">Mon enfant</Title> : 
+        <p>Titre non dispo</p>
+      }
+      <button onClick={displayTitle}>show title</button>
+      <input type="text"/>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, reiciendis aliquid ut accusantium quaerat maxime doloremque in officia aspernatur libero quas sed quisquam ipsam inventore iure distinctio totam. Reiciendis, optio.</p>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}
+      </ul>
+    </Fragment>
 }
+
+
 
 export default App
---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+function Title({color, ...props}) {
+
+    // return <h1 style={{color: color}} {...props}>{children}</h1>
+    return <h1 style={{color: color}} {...props}/>
+}
+
+export default Title
